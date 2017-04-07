d639335712fb03d4ceacf0665bbc62761ea55c5a
Peter Zhen <peterzhen0713@gmail.com>
2017-04-07 13:19:59 -0400

    add weatherService class

--- weatherService.cs
+++ weatherService.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+
 namespace Weather
 {
-	public class EmptyClass
+	public class WeatherService
 	{
-		public EmptyClass()
+		private List<City> cities;
+
+
+		public WeatherService()
 		{
+			this.totalCities = 0;
+			this.cities = new List<City>();
+		}
+
+		public void addCity(int zipcode)
+		{
+			totalCities.Add(new City(zipcode);
+		}
+
+		public void getWeather()
+		{
+			Console.WriteLine("Fetching Weather For Your Cities...");
+
+			foreach (var city in cities)
+			{
+				city.fetchWeather();
+			}
+		}
+
+		public void getCities()
+		{
+			Console.WriteLine("Your Cities: ");
+			foreach (var city in cities)
+			{
+				Console.WriteLine(city.getCity());
+			}
 		}
 	}
 }
