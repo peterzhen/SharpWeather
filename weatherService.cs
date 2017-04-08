ad9f7cdd88ce2b4fd98da506794977bc07b733d8
Peter Zhen <peterzhen0713@gmail.com>
2017-04-08 04:44:28 -0400

    refactor prompt loop

--- weatherService.cs
+++ weatherService.cs
@@ -20,58 +20,62 @@ namespace Weather
 		{
 			while (true)
 			{
-				if (this.prompting == false)
+				if (!prompting)
 				{
-					this.prompting = true;
-					if (cities.Count == 0)
-					{
-						Console.Write("To begin, please enter a 5-digit US zipcode: ");
-						string zipcode = Console.ReadLine();
-						addCity(zipcode);
-					}
-					else
-					{
-						Console.Write("What would you like to do (add, weather, cities, exit)? ");
-						string prompt = Console.ReadLine();
-						Console.WriteLine();
-						switch (prompt)
-						{
-							case "add":
-								Console.Write("Adding City - Enter a 5-digit US zipcode: ");
-								string zipcode = Console.ReadLine();
-								Console.WriteLine();
-								addCity(zipcode);
-								break;
-
-							case "weather":
-								getWeather();
-                                this.prompting = false;
-								break;
-
-							case "cities":
-								getCities();
-	                            this.prompting = false;
-								break;
-
-							case "exit":
-								Console.WriteLine("Goodbye");
-								Environment.Exit(0);
-								break;
-
-							default:
-								Console.WriteLine("Invalid Input, Try Again.\n");
-                                this.prompting = false;
-								break;
-						}
-					}
+					prompt();
 				}
 			}
+		}
+
+		private void prompt()
+		{
+			this.prompting = true;
+			if (cities.Count == 0)
+			{
+				Console.Write("To begin, please enter a 5-digit US zipcode: ");
+				string zipcode = Console.ReadLine();
+				addCity(zipcode);
+			}
+			else
+			{
+				Console.Write("What would you like to do (add, weather, cities, exit)? ");
+				string prompt = Console.ReadLine();
+				Console.WriteLine();
+				switch (prompt)
+				{
+					case "add":
+						Console.Write("Adding City - Enter a 5-digit US zipcode: ");
+						string zipcode = Console.ReadLine();
+						Console.WriteLine();
+						addCity(zipcode);
+						break;
+
+					case "weather":
+						getWeather();
+						break;
+
+					case "cities":
+						getCities();
+						break;
+
+					case "exit":
+						Console.WriteLine("Goodbye");
+						Environment.Exit(0);
+						break;
+
+					default:
+						Console.WriteLine("Invalid Input, Try Again.\n");
+						break;
+				}
+
+			}
+			this.prompting = false;
 		}
 
 		private void initMessage()
 		{
 			Console.WriteLine("************************************************************");
-			Console.WriteLine("Welcome To The Command Prompt Weather Serivce.");
+			Console.WriteLine("Welcome To The Command Prompt Weather Service.");
 			Console.WriteLine("************************************************************");
 			Console.WriteLine();
 			Console.WriteLine("To find the weather in your city, type in \"add\"");
@@ -95,7 +99,6 @@ namespace Weather
 			{
 				Console.WriteLine("\nIncorrect Zipcode, Try Again.\n");
 			}
-			this.prompting = false;
 		}
 
 		private void getWeather()
