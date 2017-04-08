420ae16fdc2569186ce03e6dd2728bf20cee0ed3
Peter Zhen <peterzhen0713@gmail.com>
2017-04-08 15:45:58 -0400

    add documentation

--- weather.cs
+++ weather.cs
@@ -4,8 +4,10 @@ namespace Weather
 {
 	class MainClass
 	{
+		// Entry Point
 		public static void Main(string[] args)
 		{
+			// Create WeatherService to start Console prompt
 			WeatherService service = new WeatherService();
 		}
 	}
